a5a2065c27b4ab8b5a0aa983704c4f3bafbcdd69
s.zinchenko <s.zinchenko@rassvet.ru>
2015-04-17 11:51:45 +0300

    Sources are added

--- assetbundles/WidgetAsset.php
+++ assetbundles/WidgetAsset.php
@@ -7,7 +7,16 @@ use yii\web\AssetBundle;
 class WidgetAsset extends AssetBundle
 {
     public $sourcePath = '@vendor/datalayerru/schedule-widget-yii2/assets';
+    public $css        = [
+        'css/angular-gantt.min.css',
+        'css/angular-gantt-plugins.css'
+    ];
     public $js         = [
+        'js/angular/angular.min.js',
+        'js/angular-moment/moment-with-locales.min.js',
+        'js/angular-moment/angular-moment.min.js',
+        'js/angular-gantt/dist/angular-gantt.min.js',
+        'js/angular-gantt/dist/angular-gantt-plugins.min.js',
         'js/app.js'
     ];
 
