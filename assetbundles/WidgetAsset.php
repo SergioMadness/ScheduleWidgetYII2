dc08e9b5fa754b3060263559ee5cea97e6e33515
s.zinchenko <s.zinchenko@rassvet.ru>
2015-04-17 11:55:32 +0300

    No angular

--- assetbundles/WidgetAsset.php
+++ assetbundles/WidgetAsset.php
@@ -12,7 +12,6 @@ class WidgetAsset extends AssetBundle
         'css/angular-gantt-plugins.css'
     ];
     public $js         = [
-        'js/angular/angular.min.js',
         'js/angular-moment/moment-with-locales.min.js',
         'js/angular-moment/angular-moment.min.js',
         'js/angular-gantt/dist/angular-gantt.min.js',
